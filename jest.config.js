1ae6a00618d1e200256c6d4d3ce02f505148e298
Yhaourt <mattis.crouzet@outlook.fr>
2025-07-04 08:55:53 +0200

    .

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  rootDir: '.',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: [
+    '<rootDir>/libs/tests/setup.js',
+    '<rootDir>/libs/tests/cleanup.js',
+  ],
+  testRegex: '.*\\.(spec|test)\\.ts$', // détecte tous les fichiers .spec.ts ou .test.ts
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['**/*.(t|j)s', '!**/node_modules/**', '!**/dist/**'],
+  coverageDirectory: './coverage',
+  roots: ['<rootDir>/apps/', '<rootDir>/libs/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleNameMapper: {
+    '^@app/common(|/.*)$': '<rootDir>/libs/common/src/$1',
+  },
+  maxWorkers: 1,
+};
